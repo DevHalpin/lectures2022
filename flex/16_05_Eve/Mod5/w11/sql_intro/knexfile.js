47639cb1199df03c08794fcaec871c367ba1c194
David Halpin <devhalpin86@gmail.com>
2022-07-26 19:23:26 -0700

    intro to sql

---
+++ flex/16_05_Eve/Mod5/w11/sql_intro/knexfile.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: process.env.PG_CONNECTION_STRING,
+    migrations: {
+      directory: __dirname + "/migrations",
+    },
+    seeds: {
+      directory: __dirname + "/seeds",
+    },
+  },
+};
